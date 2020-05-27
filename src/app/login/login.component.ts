a48c42845b8277c0f3cf8a301b413e4eb42e6cce
Sandra Binoy <sandra.binoy@outlook.com>
2020-05-27 23:20:49 +0530

    Revert "tab"
    This reverts commit 2bb1136d42560a6924f66f8430d9f2998066a58f, reversing
    changes made to 333253f7674908d5f2db61b45076b1c0fa4f25cd.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
     public service: UserDataService,
     public auth: AuthService
   ) {}
-  authenticateAdmin = () => {
+  authenticate = () => {
     if (this.service.authenticateUser(this.user)) {
       this.router.navigate(['home']);
       this.auth.changeAuth(true);
