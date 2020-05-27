163b8ed99be84cd013e2b5e9aeb5964445b2b64a
Sandra Binoy <sandra.binoy@outlook.com>
2020-05-27 22:37:53 +0530

    login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,25 +6,36 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  adminFlag: boolean = false;
+  custFlag: boolean = false;
 
   user = {
     email: '',
-    password: ''
-  }
-  constructor(public router: Router, public service: UserDataService, public auth: AuthService) { }
+    password: '',
+  };
+  constructor(
+    public router: Router,
+    public service: UserDataService,
+    public auth: AuthService
+  ) {}
   authenticate = () => {
-    if(this.service.authenticateUser(this.user)){
+    if (this.service.authenticateUser(this.user)) {
       this.router.navigate(['home']);
       this.auth.changeAuth(true);
+    } else {
+      alert('Incorrect email or password');
     }
-    else{
-      alert("Incorrect email or password")
-    }
-  }
-  ngOnInit(): void {
-  }
+  };
 
+  switchToAdmin = (adminFlag) => {
+    this.adminFlag = adminFlag;
+  };
+  switchToCust = (custFlag) => {
+    this.custFlag = custFlag;
+  };
+
+  ngOnInit(): void {}
 }
--- src/app/movies-admin/movies-admin.component.ts
+++ src/app/movies-admin/movies-admin.component.ts
@@ -4,23 +4,20 @@ import { AdminService } from '../admin.service';
 @Component({
   selector: 'app-movies-admin',
   templateUrl: './movies-admin.component.html',
-  styleUrls: ['./movies-admin.component.css']
+  styleUrls: ['./movies-admin.component.css'],
 })
 export class MoviesAdminComponent implements OnInit {
-  showModal = false
+  showModal = false;
 
-  constructor(public admin: AdminService) { }
-
+  constructor(public admin: AdminService) {}
 
   changeModal = (flag) => {
     this.showModal = flag;
-  }
+  };
   deleteMovie = (id) => {
     alert(id);
     this.admin.deleteMovie(id);
-    console.log(this.admin.movies)
-  }
-  ngOnInit(): void {
-  }
-
+    console.log(this.admin.movies);
+  };
+  ngOnInit(): void {}
 }
