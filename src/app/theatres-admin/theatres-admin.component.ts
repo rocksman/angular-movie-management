7587a53e12194ba279576894203a1f626ca27dbe
Sandra Binoy <sandra.binoy@outlook.com>
2020-05-27 16:39:02 +0530

    theatre-admin commit

--- src/app/theatres-admin/theatres-admin.component.ts
+++ src/app/theatres-admin/theatres-admin.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../admin.service';
 
 @Component({
   selector: 'app-theatres-admin',
   templateUrl: './theatres-admin.component.html',
-  styleUrls: ['./theatres-admin.component.css']
+  styleUrls: ['./theatres-admin.component.css'],
 })
 export class TheatresAdminComponent implements OnInit {
-
   showModal = false;
-  constructor(public admin: AdminService) { }
+  constructor(public admin: AdminService) {}
 
   changeModal = (flag) => {
     this.showModal = flag;
   };
-  ngOnInit(): void { }  
-
+  ngOnInit(): void {}
 }
